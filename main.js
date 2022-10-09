6a079bdad41e213304a90c2bfeeb26df21ad18be
AllynST <49324417+AllynST@users.noreply.github.com>
2022-10-09 14:31:03 +0200

    final (1 fix needed )

--- main.js
+++ main.js
@@ -1,58 +1,92 @@
-let formElem = document.querySelector("form");
+let inputContainerElem = document.querySelector("#inputContainer");
+let inputsArr = [];
+let deleteBtnArr = [];
 
-let inputsArr = document.querySelectorAll("input");
+let MaxSpan = document.querySelector("#MaxSpan");
+let MinSpan = document.querySelector("#MinSpan");
+let AvgSpan = document.querySelector("#AvgSpan");
+let SumSpan = document.querySelector("#SumSpan");
 
-const addEventListeners = () =>{
-    inputsArr.forEach(elem =>{
+const addEventListeners = () => {
+    inputsArr.forEach((elem) => {
+        elem.addEventListener("input", () => {
 
+            MaxSpan.innerHTML = `Max= ${getMaxNum(inputsArr)}`;
+            MinSpan.innerHTML = `Min= ${getMinNum(inputsArr)}`; 
+            AvgSpan.innerHTML = `Avg=${getAvg(inputsArr)}`;
+            SumSpan.innerHTML = `Sum=${getSum(inputsArr)}`;
         
-elem.addEventListener("keypress",()=>{
-    console.log("any input is pressed")
- getMaxNum();
-});
+
+
+        });
+    });
+    deleteBtnArr.forEach(elem =>{
+        elem.addEventListener("click", ()=>{
+            removeInput(+elem.value);
+        })
     })
+};
+
+const removeInput = index =>{   
+
+    inputContainerElem.removeChild(deleteBtnArr[index]);
+    inputContainerElem.removeChild(inputsArr[index]);
+ 
 }
 
+const addField = () => {
+    let input = document.createElement("input");
+    let deleteBtn = document.createElement("button");
+    deleteBtn.value = inputsArr.length
+    console.log(deleteBtn.value);
+    deleteBtn.innerText="X"
+    deleteBtn.className = 'deleteBtn';
+    inputContainerElem.appendChild(input);
+    inputContainerElem.appendChild(deleteBtn);
+    
 
-
-
-
-const addField = () =>{
-    let label = document.createElement("input");
-    label.className="inputLabel";
-    formElem.appendChild(label);
-
-
-    inputsArr = document.querySelectorAll("input");
+    inputsArr = Array.from(document.querySelectorAll("input"));
+    deleteBtnArr = Array.from(document.querySelectorAll(".deleteBtn"))
     addEventListeners();
-    
-    
-}
+};
 const addFieldBtn = document.getElementById("addFieldBtn");
 const maxBtn = document.querySelector("MaxBtn");
 
+const getMaxNum = (inputsArr) => {
+     inputsArr = getValueFromInput(inputsArr);
 
-const getMaxNum = inputsArr =>{
-    console.log(inputsArr)
-    let tempArr = []
+    return Math.max(...inputsArr);
+};
+//
 
-    for(let i in inputsArr){
-        tempArr.push(inputsArr[i].value ?? 0)
-    }    
-      return Math.max(...tempArr)
+const getAvg = inputsArr =>{
+    inputsArr = getValueFromInput(inputsArr);
+    return inputsArr.reduce((a, b) => a + b, 0) / inputsArr.length;
+}
+
+const getSum = inputsArr =>{
+    inputsArr = getValueFromInput(inputsArr);
+
+    return inputsArr.reduce((a, b) => a + b, 0);
+ 
+}
+
+const getMinNum = inputsArr =>{
+   
+   inputsArr = getValueFromInput(inputsArr);
+   return Math.min(...inputsArr);
+}
+
+const getValueFromInput = inputArr =>{
+    
+    const targetArr = inputsArr.map(elem=>{
+    return +elem.value ?? 0;
+   })
+
+   return targetArr;
 }
 
 
-MaxBtn.addEventListener("click",()=>{
-console.log(getMaxNum(inputsArr));
-})
-
-
-addFieldBtn.addEventListener("click",()=>{
-addField(); 
-})
-
-
-
-
-
+addFieldBtn.addEventListener("click", () => {
+    addField();
+});
