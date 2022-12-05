837a3dc101ee85409e5346311ebf9003178a3ecd
AllynST <49324417+AllynST@users.noreply.github.com>
2022-12-05 03:45:15 +0100

    small refactor added besier curve template for lab 8 (bugged)

--- Lab8 Canvas/main.js
+++ Lab8 Canvas/main.js
@@ -1,5 +1,19 @@
 import Particle from "./particle.js";
 
+
+let quantitySlider = document.querySelector("#ballsQ");
+let rangeSlider = document.querySelector("#conRangeValue");
+
+let eatStrengthSlider = document.querySelector("#eatStrength");
+let mousePushSlider = document.querySelector("#mousePush")
+export const colorPallete =[
+    '#219EBC',
+    '#023047',
+    '#FFB703',
+    '#FB8500',
+    '#06d6a0'
+]
+
 let canvas = document.createElement("canvas");
 
 canvas.width = window.innerWidth;
@@ -7,98 +21,194 @@ canvas.height = window.innerHeight;
 
 document.querySelector("#root").append(canvas);
 
-let n = 500; // number of particles
-let particles = [];
-let ctx = canvas.getContext("2d");
-
-for (let i = 0; i < n; i++) {
-    particles.push(new Particle());
-}
+document.querySelector("#resetBtn").addEventListener("click",()=>{
+    reset();
+})
 
 let mouseX;
 let mouseY;
-let eatSpeed= 0.1;
-let eatDistance = 100;
+let eatSpeed= 0.05;
+let mousePushDistance = 100;
+let connectRadius = 200;
 
-animate();
+let ctx;
+let n;
+let particles = [];
+
+reset();
+
+function reset(){
+     n = quantitySlider.value;
+     //n=5;
+    eatSpeed = eatStrengthSlider.value/10;
+    connectRadius = rangeSlider.value;
+    mousePushDistance = mousePushSlider.value;
+    particles = [];
+    
+    ctx = canvas.getContext("2d");
+    
+    for (let i = 0; i < n; i++) {
+        particles.push(new Particle());
+    }
+    animate();
+}
 
 function animate() {
 
     ctx.beginPath();
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "#8ECAE6";
 
     ctx.rect(0, 0, window.innerWidth, window.innerHeight);
     ctx.fill();
     ctx.stroke();
+    
+    particles.forEach((ballOne,indexOne) => {
+        ballOne.connectedIndexes = [];
+         
+        
+        particles.forEach((ballTwo,indexTwo) => {
 
-    particles.forEach((particle) => {
-        particle.render(ctx);
-
-        particles.forEach((elem) => {
-            if (particle != elem) {
-                const xDif = elem.position.x - particle.position.x;
-                const yDif = elem.position.y - particle.position.y;
+            let bezierColor;
+            if (ballOne != ballTwo) {
+                const xDif = ballTwo.position.x - ballOne.position.x;
+                const yDif = ballTwo.position.y - ballOne.position.y;
 
                 const distance = Math.sqrt(
                     Math.pow(xDif, 2) + Math.pow(yDif, 2)
                 );
 
-                if (distance < 200) {
-                    if(elem.radius >particle.radius){
-                        elem.radius +=eatSpeed;
-                        particle.radius-=eatSpeed
+                if (distance < connectRadius) {
+                    if(!(ballOne.isConnected(indexTwo))){
+
+                    ballOne.connectedIndexes.push(indexTwo);
+
                         
-                    }
-                    else if(elem.radius <particle.radius){
-                        elem.radius -= eatSpeed;
-                        particle.radius += eatSpeed;
+                    
+                    shrink(indexOne,indexTwo); 
+
+                    ctx.globalAlpha = ((connectRadius - distance) / distance);
+                    let xModif
+                    let yModif
                         
+                    if(xDif>yDif){                        
+                        xModif = 0;
+                        yModif = 1;
                     }
-                    if(elem.radius <=1){
-                        particles.splice(particles.indexOf(elem), 1);
+                    else{                        
+                        xModif = 1;
+                        yModif = 0;
                     }
-                    if(particle.radius <=1){
-                        particles.splice(particles.indexOf(particle), 1);
-                    }
-                    ctx.globalAlpha = (200 - distance) / 200;
+                    
+                    
                     ctx.beginPath();
-                    ctx.moveTo(elem.position.x, elem.position.y);
-                    ctx.lineTo(particle.position.x, particle.position.y);
+                    ctx.moveTo(ballOne.position.x,
+                           ballOne.position.y)
+
+                    ctx.lineTo(ballTwo.position.x,
+                    ballTwo.position.y,)
+
+                    //  ctx.moveTo(ballOne.position.x + (xModif == 0 ? 0 : -ballOne.radius),
+                    //       ballOne.position.y + (yModif == 0 ? 0 : -ballOne.radius));
+                    // ctx.bezierCurveTo(
+                    //     ballOne.position.x + (xModif == 0 ? 0 : -ballOne.radius),
+                    //     ballOne.position.y + (yModif == 0 ? 0 : -ballOne.radius),
+
+                    //     (ballOne.position.x + ballTwo.position.x)/2,
+                    //     (ballOne.position.y + ballTwo.position.y)/2,
+
+                    //     ballTwo.position.x + (xModif == 0 ? 0 : -ballTwo.radius),
+                    //     ballTwo.position.y + (yModif == 0 ? 0 : -ballTwo.radius)
+                    // );
+
+                    // ctx.lineTo(ballOne.position.x + (xModif == 0 ? 0 : +ballOne.radius),
+                    // ballOne.position.y + (yModif == 0 ? 0 : +ballOne.radius),)
+                    
+                    // ctx.bezierCurveTo(
+                    //     ballOne.position.x + (xModif == 0 ? 0 : +ballOne.radius),
+                    //     ballOne.position.y + (yModif == 0 ? 0 : +ballOne.radius),
+
+                    //     (ballOne.position.x + ballTwo.position.x)/2,
+                    //     (ballOne.position.y + ballTwo.position.y)/2,
+
+                    //     ballTwo.position.x + (xModif == 0 ? 0 : +ballTwo.radius),
+                    //     ballTwo.position.y + (yModif == 0 ? 0 : +ballTwo.radius)
+                    // );
+
+                    ctx.fillStyle = bezierColor;
+                  
+
                     ctx.stroke();
+                    ctx.fill()                    
+                    
+                    
+                    
                     ctx.globalAlpha = 1;
+                    }
+                    
                 }
+                
+                
             }
-        });
-    });
-    particles.forEach((particle) => {
-        const xDif = particle.position.x - mouseX;
-        const yDif = particle.position.y - mouseY;
+            
+        });    
+        
+        const xDif = ballOne.position.x - mouseX;
+        const yDif = ballOne.position.y - mouseY;
 
         const distance = Math.sqrt(Math.pow(xDif, 2) + Math.pow(yDif, 2));
 
-        ctx.beginPath();
-        ctx.arc(mouseX, mouseY, 300, 0, 2 * Math.PI);
-        ctx.stroke();
-
-        if (distance < eatDistance) {
-            const xModifier = ((eatDistance - distance) * (xDif > 0 ? 1  : -1))*0.05 + particle.velocity.x*-1;
-            const yModifier = ((eatDistance - distance) * (yDif > 0 ? 1  : -1))*0.05 + particle.velocity.y*-1;
+        if (distance < mousePushDistance) {
+            const xModifier = ((mousePushDistance - distance) * (xDif > 0 ? 1  : -1))*0.05 + ballOne.velocity.x*-1;
+            const yModifier = ((mousePushDistance - distance) * (yDif > 0 ? 1  : -1))*0.05 + ballOne.velocity.y*-1;
                
                 
 
-                 particle.velocityModif.x = xModifier;
-                 particle.velocityModif.y = yModifier;
+                 ballOne.velocityModif.x = xModifier;
+                 ballOne.velocityModif.y = yModifier;
                 
         } else {
-            particle.velocityModif.x = 0;
-            particle.velocityModif.y = 0;
+            ballOne.velocityModif.x = 0;
+            ballOne.velocityModif.y = 0;
         }
+        ballOne.render(ctx);
     });
-   
+    
     requestAnimationFrame(animate);
+}
+
+export function removeBall(ball){
+    console.log("ball removed")
+    const index = particles.indexOf(ball);
+    particles.splice(index,1)
 }
 
 canvas.addEventListener("mousemove", (e) => {
     mouseX = e.offsetX;
     mouseY = e.offsetY;
 });
+
+
+function shrink(index1,index2){
+    
+    if(particles[index2].radius >particles[index1].radius){
+        
+        
+        if(particles[index2].radius<window.innerHeight/4){
+            particles[index2].radius +=eatSpeed;
+            
+        }
+        particles[index1].radius-=eatSpeed
+        
+        
+    }
+    else if(particles[index2].radius <particles[index1].radius){
+        
+        if(particles[index1].radius<window.innerHeight/4){
+            
+            
+            particles[index1].radius += eatSpeed;
+        }
+        particles[index2].radius -= eatSpeed;
+    }
+
+}
--- Lab8 Canvas/particle.js
+++ Lab8 Canvas/particle.js
@@ -1,4 +1,11 @@
+import { colorPallete,removeBall } from "./main.js";
+
 export default class Particle {
+    
+    color = colorPallete[getRandomInt(colorPallete.length-1)]
+
+    connectedIndexes = []
+
     position = {
         x: 0,
         y: 0,
@@ -9,16 +16,22 @@ export default class Particle {
         y:0
     }
 
-    radius = 5;
+    radius = 50;
     velocity = {
         x: 2,
         y: 1,
     };
 
-    constructor() {
+    conDistance;
+
+    constructor() {        
+       
         this.radius = getRandomInt(15) + 5;
         this.velocity.x = getRandomInt(10)-5;
         this.velocity.y = getRandomInt(10)-5;
+
+        //  this.velocity.x = 0;
+        //  this.velocity.y = 0;
   
         this.position.x = getRandomInt(window.innerWidth);
         this.position.y = getRandomInt(window.innerHeight);
@@ -26,9 +39,23 @@ export default class Particle {
     }
 
     velocityHandler() {
-        this.position.x += ((this.velocity.x)/(this.radius*0.1)) +this.velocityModif.x ;
+        this.position.x += (this.velocity.x/(this.radius*0.1)) +this.velocityModif.x ;
         this.position.y +=( this.velocity.y / (this.radius * 0.1)) + this.velocityModif.y;
 
+    }
+
+    checkConnections = () => {        
+        
+    }
+
+    isConnected = (ball) =>{        
+        return this.connectedIndexes.includes(ball)
+    }
+
+    checkForRemoval = () =>{
+        if(this.radius < 1){
+            removeBall(this)
+        }
         
     }
     
@@ -46,7 +73,7 @@ export default class Particle {
         if (this.position.y < 0+this.radius ) {
             this.position.y = 0+this.radius;
             this.velocity.y = this.velocity.y *-1;
-      }
+        }
         if(this.position.y > window.innerHeight - this.radius){
             this.position.y = window.innerHeight - this.radius;
             this.velocity.y = this.velocity.y *-1;
@@ -55,16 +82,27 @@ export default class Particle {
 
     render(context) {
         context.beginPath();
-        context.arc(
-            this.position.x,
-            this.position.y,
-            this.radius,
-            0,
-            2 * Math.PI
-        );
+        this.checkForRemoval();   
+             
+        context.fillStyle = `${this.color}`;
+
+        if(this.radius>0){
+            context.arc(
+                this.position.x,
+                this.position.y,
+                this.radius,
+                0,
+                2 * Math.PI
+            );
+        }
+             
+        
+        
+        context.fill();
         context.stroke();
-this.bounce();
+        this.bounce();
         this.velocityHandler();
+        
         
     }
 }
