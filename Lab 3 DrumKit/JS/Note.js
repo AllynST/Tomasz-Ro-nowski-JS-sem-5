9330e242daf3567f71aa92b60cb5dd0320500435
AllynST <tomaszroznowskii@gmail.com>
2023-05-11 18:34:19 +0200

    post semester cleanup

--- Lab 3 DrumKit/JS/Note.js
+++
@@ -1,9 +0,0 @@
-//This class holds only data about played note
-//its purpouse is to cleanup unnecesary variable clutter and to simplify code
-export class Note {
-    constructor(startTime, keyCode, duration) {
-        this.startTime = startTime;
-        this.duration = duration;
-        this.keyCode = keyCode;
-    }
-}
--- Lab 3 DrumKit/JS/main.js
+++
@@ -1,6 +0,0 @@
-import { startUP } from "./startUp.js";
-startUP();
-export let letters = [];
-for (var i = 65; i <= 90; i++) {
-    letters.push(String.fromCharCode(i));
-}
